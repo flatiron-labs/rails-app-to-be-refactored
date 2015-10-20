0e3a170d744ec330f8954188891b284446d25c46
Drew Price <drewprice@users.noreply.github.com>
2015-10-20 18:32:38 -0400

    Run Admin migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151020002217) do
+ActiveRecord::Schema.define(version: 20151020222852) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "admins", ["email"], name: "index_admins_on_email", unique: true
+  add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
 
   create_table "picture_comments", force: :cascade do |t|
     t.integer "picture_id"
@@ -20,11 +38,6 @@ ActiveRecord::Schema.define(version: 20151020002217) do
 
   create_table "pictures", force: :cascade do |t|
     t.string "url"
-  end
-
-  create_table "pictures_users", id: false, force: :cascade do |t|
-    t.integer "user_id",    null: false
-    t.integer "picture_id", null: false
   end
 
   create_table "user_pictures", force: :cascade do |t|
@@ -46,7 +59,6 @@ ActiveRecord::Schema.define(version: 20151020002217) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
-    t.integer  "money"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
