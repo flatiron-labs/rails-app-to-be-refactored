8d840408a8a68a0e2ad2712a7712b814343715eb
Drew Price <drewprice@users.noreply.github.com>
2015-10-20 20:44:36 -0400

    Refactor views

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,10 +1,8 @@
 class Picture < ActiveRecord::Base
   belongs_to :user
-
   has_many :comments
 
   # TODO: improve format regex
   validates :url, presence: true,
                   format: { with: /https?:\/\/.+/i, on: :create }
-
 end
